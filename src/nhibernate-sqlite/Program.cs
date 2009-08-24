b0ef74c77e5ff982639c7fd3a12b89ef593496d7
Richard Hubers <richard.hubers@gmail.com>
2009-08-24 17:10:59 +0200

    Updated the NHibernate example with StatelessSession.

--- src/nhibernate-sqlite/Program.cs
+++ src/nhibernate-sqlite/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using NHibernate;
 using NHibernate.Cfg;
-using NHibernate.Criterion;
 using NHibernate.Tool.hbm2ddl;
 using Spikes.Model;
 using Environment=NHibernate.Cfg.Environment;
@@ -23,7 +22,8 @@ namespace Spikes
 				properties[Environment.ConnectionString] = "Data Source=test.db;Version=3;New=False;Compress=True;";
 				properties[Environment.QuerySubstitutions] = "true=1;false=0";
 				properties[Environment.ReleaseConnections] = "on_close";
-				// properties[Environment.ShowSql] = "true";
+				properties[Environment.BatchSize] = "1000";
+				//properties[Environment.ShowSql] = "true";
 
 				Configuration cfg = new Configuration();
 				cfg.Properties = properties;
@@ -33,27 +33,59 @@ namespace Spikes
 				new SchemaExport(cfg).Execute(true, true, false, true);
 				Console.WriteLine();
 
-				const int count = 4;
+				const int StatelessCount = 100000;
+				const int Count = 4;
+
+				ISessionFactory factory = cfg.BuildSessionFactory();
+
+				using (IStatelessSession session = factory.OpenStatelessSession())
+				{
+					using (ITransaction transaction = session.BeginTransaction())
+					{
+						Node node = new Node("first");
+						session.Insert(node);
+						transaction.Commit();
+					}
+				}
 
 				Stopwatch stopwatch = Stopwatch.StartNew();
 
-				ISessionFactory factory = cfg.BuildSessionFactory();
+				using (IStatelessSession session = factory.OpenStatelessSession())
+				{
+					using (ITransaction transaction = session.BeginTransaction())
+					{
+						for (int i = 0; i < StatelessCount; i++)
+						{
+							Node node = new Node(i.ToString());
+							session.Insert(node);
+						}
+						transaction.Commit();
+					}
+				}
+				stopwatch.Stop();
+				Console.WriteLine("{0} records (flat)", StatelessCount);
+				Console.WriteLine("Inserting: {0,4} ms", stopwatch.ElapsedMilliseconds);
+
+				List<Guid> ids = new List<Guid>();
+
+				stopwatch.Reset();
+				stopwatch.Start();
 				using (ISession session = factory.OpenSession())
 				{
 					using (ITransaction transaction = session.BeginTransaction())
 					{
-						for (int i = 0; i < count; i++)
+						for (int i = 0; i < Count; i++)
 						{
 							Node inode = new Node(i.ToString());
-							for (int j = 0; j < count; j++)
+							for (int j = 0; j < Count; j++)
 							{
 								Node jnode = new Node(j.ToString());
 								inode.Add(jnode);
-								for (int k = 0; k < count; k++)
+								for (int k = 0; k < Count; k++)
 								{
 									Node knode = new Node(k.ToString());
 									jnode.Add(knode);
-									for (int l = 0; l < count; l++)
+									for (int l = 0; l < Count; l++)
 									{
 										Node lnode = new Node(l.ToString());
 										knode.Add(lnode);
@@ -61,14 +93,15 @@ namespace Spikes
 								}
 							}
 							session.Save(inode);
+							ids.Add(inode.Id);
 						}
 						transaction.Commit();
 					}
 				}
 
 				stopwatch.Stop();
-				Console.WriteLine("{0} records: {1} ms", count * count * count * count, stopwatch.ElapsedMilliseconds);
-				Console.WriteLine();
+				Console.WriteLine("{0} records (hierarchical)", Count * Count * Count * Count);
+				Console.WriteLine("Inserting: {0,4} ms", stopwatch.ElapsedMilliseconds);
 
 				using (ISession session = factory.OpenSession())
 				{
@@ -77,24 +110,23 @@ namespace Spikes
 						stopwatch.Reset();
 						stopwatch.Start();
 
-						ICriteria criteria = session.CreateCriteria(typeof(Node));
-						criteria.Add(Expression.IsNull("Parent"));
-
-						// This may actually slow down the total time:
-						// loading will increase more than looping will decrease
-						criteria.SetFetchMode("Children", FetchMode.Eager);
-						criteria.SetFetchMode("Children.Children", FetchMode.Eager);
-						criteria.SetFetchMode("Children.Children.Children", FetchMode.Eager);
-						criteria.SetFetchMode("Children.Children.Children.Children", FetchMode.Eager);
-						Node node = criteria.List<Node>()[0];
+						IQuery query = session.CreateQuery("from Node n where n.Id in (:ids) and n.Parent is null and size(n.Children) > 0");
+						query.SetParameterList("ids", ids);
+						IList<Node> nodes = query.List<Node>();
+						//Node node = criteria.List<Node>()[0];
 						stopwatch.Stop();
-						Console.WriteLine("Loading: {0} ms", stopwatch.ElapsedMilliseconds);
+						Console.WriteLine("Loading:   {0,4} ms", stopwatch.ElapsedMilliseconds);
 
-						stopwatch.Reset();
-						stopwatch.Start();
-						Loop(node, 0);
-						stopwatch.Stop();
-						Console.WriteLine("Looping: {0} ms", stopwatch.ElapsedMilliseconds);
+						foreach (Node node in nodes)
+						{
+							stopwatch.Reset();
+							stopwatch.Start();
+							int total = 0;
+							Loop(node, 0, delegate { total++; });
+							stopwatch.Stop();
+							Console.WriteLine("{0} records (hierarchical)", total);
+							Console.WriteLine("Looping:   {0,4} ms", stopwatch.ElapsedMilliseconds);
+						}
 
 						transaction.Commit();
 					}
@@ -107,12 +139,15 @@ namespace Spikes
 			}
 		}
 
-		private static void Loop(Node node, int level)
+		public delegate void Action();
+
+		private static void Loop(Node node, int level, Action action)
 		{
-			Console.WriteLine("{0}{1}...: {2}", new string(' ', level), node.Id.ToString().Substring(0, 8), node.Name);
+			action();
+			//Console.WriteLine("{0}{1}...: {2}", new string(' ', level), node.Id.ToString().Substring(0, 8), node.Name);
 			foreach (Node child in node.Children)
 			{
-				Loop(child, level + 2);
+				Loop(child, level + 2, action);
 			}
 		}
 	}
